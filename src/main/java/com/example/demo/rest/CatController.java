0f4783b307bd01a0c0ecac2207ae674cf0801f23
44jovi <kwong.jovi@gmail.com>
2023-04-19 10:20:23 +0100

    add get()

--- src/main/java/com/example/demo/rest/CatController.java
+++ src/main/java/com/example/demo/rest/CatController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +31,13 @@ public class CatController {
 	@GetMapping("/getAll")
 	public List<Cat> getAll() {
 		return this.cats;
+	}
+	
+	@GetMapping("/get/{id}")
+	// if variable names don't match, you'd have to add it after @PathVariable
+	public Cat get(@PathVariable int id) {
+		
+		return this.cats.get(id);
+		 
 	}
 }
