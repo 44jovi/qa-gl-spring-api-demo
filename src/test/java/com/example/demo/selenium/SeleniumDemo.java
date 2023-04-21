f2b184a56e4737d7efe1aa21bd536db11506a68c
44jovi <kwong.jovi@gmail.com>
2023-04-21 10:09:18 +0100

    progress - pass turtleTest

--- src/test/java/com/example/demo/selenium/SeleniumDemo.java
+++ src/test/java/com/example/demo/selenium/SeleniumDemo.java
@@ -3,7 +3,10 @@ package com.example.demo.selenium;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,10 +21,11 @@ public class SeleniumDemo {
 	@BeforeEach
 	void init() {
 		// Allows Selenium to access Chrome browser dev tools
+		// Pass it into the driver
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 		// Create new browser window
-		this.driver = new ChromeDriver();
+		this.driver = new ChromeDriver(options);
 		this.driver.manage().window().maximize();
 	}
 	
@@ -33,6 +37,12 @@ public class SeleniumDemo {
 	void turtleTest() {
 		// Redirect browser to the URL
 		this.driver.get("https://www.bbc.co.uk/search");
+		
+		// Can also use By.id but By.cssSelector is more generic approach
+		WebElement search = this.driver.findElement(By.cssSelector("#search-input"));
+//		search.getRect();
+		search.sendKeys("turtle");
+		search.sendKeys(Keys.ENTER);
 	}
 	
 }
